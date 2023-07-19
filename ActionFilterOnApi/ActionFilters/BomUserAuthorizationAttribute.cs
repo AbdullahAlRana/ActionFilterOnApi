d39446f263ca558051e85e1a6cc55e909ac6fc14
Md Abdullah <MAbdullah@rensafamily.company>
2023-07-19 10:21:25 +0600

    Added the action filter

---
+++ ActionFilterOnApi/ActionFilters/BomUserAuthorizationAttribute.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Diagnostics;
+
+namespace ActionFilterOnApi.ActionFilters
+{
+    public class BomUserAuthorizationAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            Log("OnActionExecuting", context.RouteData);
+        }
+
+        public override void OnActionExecuted(ActionExecutedContext context)
+        {
+            Log("OnActionExecuted", context.RouteData);
+        }
+
+        public override void OnResultExecuting(ResultExecutingContext context)
+        {
+            Log("OnResultExecuting", context.RouteData);
+        }
+
+        public override void OnResultExecuted(ResultExecutedContext context)
+        {
+            Log("OnResultExecuted", context.RouteData);
+        }
+
+        private static void Log(string methodName, RouteData routeData)
+        {
+            var controllerName = routeData.Values["controller"];
+            var actionName = routeData.Values["action"];
+            var message = string.Format("{0} controller:{1} action:{2}", methodName, controllerName, actionName);
+            Console.WriteLine(message, "Action Filter Log");
+        }
+    }
+}
--- ActionFilterOnApi/Controllers/WeatherForecastController.cs
+++ ActionFilterOnApi/Controllers/WeatherForecastController.cs
@@ -1,15 +1,17 @@
+using ActionFilterOnApi.ActionFilters;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ActionFilterOnApi.Controllers
 {
+    [BomUserAuthorization]
     [ApiController]
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
@@ -18,7 +20,7 @@ namespace ActionFilterOnApi.Controllers
             _logger = logger;
         }
 
-        [HttpGet(Name = "GetWeatherForecast")]
+        [HttpGet("GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
